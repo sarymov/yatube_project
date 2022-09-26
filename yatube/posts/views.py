cfdc1e91376db06d6b8d2c346e5e6af69bafdb87
Сергей Сарымов <sarymovss@gmail.com>
2022-09-26 20:35:00 +0300

    Словарь контекста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,21 +3,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    TEMPLATE = 'posts/index.html'
-    TITLE = 'Это главная страница проекта Yatube'
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
     context = {
-        'title': TITLE,
+        'title': title,
     }
-    return render(request, TEMPLATE, context)
+    return render(request, template, context)
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    TEMPLATE_2 = 'posts/group_list.html'
-    TITLE_2 = 'Здесь будет информация о группах проекта Yatube'
-    context_2 = {
-        'title_2': TITLE_2
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title
     }
-    return render(request, TEMPLATE_2, context_2)
+    return render(request, template, context)
 
 # Create your views here.
