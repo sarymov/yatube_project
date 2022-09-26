14aa87eb3fdf49df54e4a4c0b7c866f4953149b1
Сергей Сарымов <sarymovss@gmail.com>
2022-09-26 17:09:56 +0300

    Работает лого, ура

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,10 +1,12 @@
 # posts/urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='main_page'),
     path('group/<slug:slug>', views.group_posts, name='group_posts')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
